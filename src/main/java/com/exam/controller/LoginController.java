868d4023f44fe3d870e24e312ec80a57a60a523e
dagyung-yang <didekrud1@gmail.com>
2024-06-14 16:17:00 +0900

    first commit

---
+++ src/main/java/com/exam/controller/LoginController.java
@@ -0,0 +1,41 @@
+package com.exam.controller;
+
+
+import javax.validation.Valid;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.exam.dto.Member;
+import com.exam.service.MemberService;
+
+
+@Controller
+public class LoginController {
+
+	private Logger logger = LoggerFactory.getLogger(getClass());
+	
+	@GetMapping(value={"/login"})
+	public String showLoginPage() {
+		
+		return "loginForm";
+	}
+
+	@GetMapping(value={"/login_fail"})
+	public String showlogin_failPage() {
+		logger.info("logger:showlogin_failPage");
+		return "redirect:login";
+	}
+
+	@GetMapping(value={"/login_success"})
+	public String showlogin_successPage() {
+		logger.info("logger:showlogin_successPage");
+		return "redirect:home";
+	}
+}
